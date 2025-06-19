9bfb608e056b0c3dce491760360fd243852b30ac
arplv <dimitrii.mueller@gmail.com>
2025-06-19 16:01:48 +0200

    test: change the parameters of the training

--- src/main.py
+++ src/main.py
@@ -179,17 +179,17 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="FastRP for Heterogeneous Graphs")
     parser.add_argument('--data-dir', type=str, default='data', help='Directory containing the dataset')
     parser.add_argument('--meta-paths', type=str, nargs='+', 
-                        default=['AAA', 'ACA', 'ATA', 'ACA@ATA'], 
+                        default=['AAA', 'ACA', 'ATA'], 
                         help='List of meta-paths to use for feature generation.')
-    parser.add_argument('--dim', type=int, default=128, help='Embedding dimension.')
-    parser.add_argument('--num-powers', type=int, default=2, help='Number of matrix powers to use for each meta-path feature.')
+    parser.add_argument('--dim', type=int, default=512, help='Embedding dimension.')
+    parser.add_argument('--num-powers', type=int, default=4, help='Number of matrix powers to use for each meta-path feature.')
     parser.add_argument('--alpha', type=float, default=-0.5, help='Exponent for degree weighting of the random projection matrix.')
     parser.add_argument('--beta', type=float, default=-1.0, help='Exponent for degree normalization of the meta-path matrix.')
     parser.add_argument('--epochs', type=int, default=30, help='Number of training epochs.')
     parser.add_argument('--lr', type=float, default=0.01, help='Learning rate.')
     parser.add_argument('--entropy-beta', type=float, default=0.05, help='Coefficient for entropy regularization term.')
-    parser.add_argument('--neg-samples', type=int, default=3, help='Number of negative samples per positive sample.')
-    parser.add_argument('--batch-size', type=int, default=4096, help='Training batch size')
+    parser.add_argument('--neg-samples', type=int, default=15, help='Number of negative samples per positive sample.')
+    parser.add_argument('--batch-size', type=int, default=64, help='Training batch size')
     parser.add_argument('--device', type=str, default='auto', help='Device to use for training (e.g., "cpu", "cuda", "mps", "auto").')
     parser.add_argument('--output', type=str, default='author_embeddings.npy', help='Path to save final embeddings')
     parser.add_argument('--seed', type=int, default=42, help='Random seed for reproducibility.')
